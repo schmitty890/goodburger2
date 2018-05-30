dee133b305762763310288f7a1242621dd186211
Jason Schmitt <schmitty890@gmail.com>
2018-05-30 12:13:57 -0400

    placeholder for burger model

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+// // Create a Burger model with columns
+// module.exports = function(sequelize, DataTypes) {
+//   var Burger = sequelize.define("Burger", {
+//     text: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: [1, 140]
+//       }
+//     },
+//     complete: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     },
+//     name: {
+//       type: DataTypes.STRING
+//     }
+//   });
+//   return Burger;
+// };
