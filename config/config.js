511a79e27f0de0d3b478f8f3927f6123e5cd2c38
Jason Schmitt <schmitty890@gmail.com>
2018-06-03 12:53:51 -0400

    moving dotenv to server.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,3 @@
-const dotenv = require('dotenv').config();
-
 module.exports = {
   development: {
     username: "root",
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var dotenv = require('dotenv').config();
 
 var app = express();
 var PORT = process.env.PORT || 8080;
