fb45e2c24cefd0d84e5844d38eb1ae4fafdcc3c5
Jason Schmitt <schmitty890@gmail.com>
2018-06-03 12:15:06 -0400

    fixing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
 gulp.task('styles', function() {
   gulp.src('sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
