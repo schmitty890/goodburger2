08ca36931eee62e02f7f80584e15222006d10939
Jason Schmitt <schmitty890@gmail.com>
2018-06-03 21:30:45 -0400

    adding sourcemaps for scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,41 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cssMin = require('gulp-css');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean');
+var maps = require('gulp-sourcemaps');
 
+// remove dist and public assets css
+gulp.task('clean', function() {
+  console.log('gulp clean task');
+  gulp.src([
+    'public/assets/build'
+      ])
+      .pipe(clean());
+});
+
+// convert sass into css, concatenate and minify. add it to build/css folder
 gulp.task('styles', function() {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/assets/css/'));
+  console.log('gulp styles task');
+  gulp.src('sass/styles.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    // .pipe(concat('app.css'))
+    // .pipe(cssMin())
+    .pipe(gulp.dest('public/assets/build/css'));
 });
 
-//Watch task
-gulp.task('default',function() {
-  gulp.watch('sass/**/*.scss',['styles']);
+// concat and minify public assets js scripts that end in .js
+gulp.task('scripts', function() {
+  console.log('gulp scripts task');
+  gulp.src([
+    './public/assets/js/*.js'
+      ])
+      .pipe(concat('app.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('public/assets/build/js'));
 });
+
+gulp.task('default', ['clean', 'scripts', 'styles']);
