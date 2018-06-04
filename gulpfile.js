8bd28d3f44690a526719214a2308d86b43914b43
Jason Schmitt <schmitty890@gmail.com>
2018-06-03 23:03:24 -0400

    adding gulp minified versions to document

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var cssMin = require('gulp-css');
 var sass = require('gulp-sass');
 var clean = require('gulp-clean');
 var maps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
 
 // remove dist and public assets css
 gulp.task('clean', function() {
@@ -17,8 +18,8 @@ gulp.task('clean', function() {
 
 // convert sass into css, concatenate and minify. add it to build/css folder
 gulp.task('compileSass', function() { // clean is a dependency of compileSass
-  console.log('gulp styles task');
-  gulp.src('sass/styles.scss')
+  console.log('gulp compileSass task');
+  return gulp.src('sass/styles.scss')
     .pipe(maps.init())
     .pipe(sass())
     .pipe(maps.write('./'))
@@ -28,9 +29,9 @@ gulp.task('compileSass', function() { // clean is a dependency of compileSass
 });
 
 // concat and minify public assets js scripts that end in .js
-gulp.task('scripts', function() {
-  console.log('gulp scripts task');
-  gulp.src([
+gulp.task('concatScripts', function() {
+  console.log('gulp concatScripts task');
+  return gulp.src([
     './public/assets/js/*.js'
       ])
       .pipe(maps.init())
@@ -40,11 +41,35 @@ gulp.task('scripts', function() {
       .pipe(gulp.dest('public/assets/build/js'));
 });
 
-gulp.task('watchSass', function() {
-  gulp.watch('sass/**/*.scss', ['compileSass']);
+gulp.task('minifyCSS', ['compileSass'], function() {
+  console.log('gulp minifyCSS task');
+  gulp.src([
+    'public/assets/build/css/styles.css'
+    ])
+    .pipe(cssMin())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('public/assets/build/css/'));
 })
 
-gulp.task('build', ['scripts', 'compileSass']);
+gulp.task('minifyScripts', ['concatScripts'], function() {
+  console.log('gulp minifyScripts task');
+  gulp.src([
+    'public/assets/build/js/app.js'
+    ])
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('public/assets/build/js/'));
+});
+
+gulp.task('watchFiles', function() {
+  console.log('gulp watchFiles task');
+  gulp.watch('sass/**/*.scss', ['compileSass']);
+  gulp.watch('public/assets/js/*.js', ['concatScripts']);
+})
+
+gulp.task('serve', ['watchFiles']);
+
+gulp.task('build', ['concatScripts', 'compileSass', 'minifyScripts', 'minifyCSS']);
 
 
 gulp.task('default', ['clean'], function() {
